5a4993ca26d694e9b3b5c401c80e2c3ebfd24647
Nguyễn Nhật Anh <ace2002luffy@gmail.com>
2023-03-03 00:55:47 +0700

    2d UI

--- readme.md
+++ readme.md
@@ -2,7 +2,5 @@ Bloxor game with AI
 
 assets can be found from http://www.flaticon.com with 64x64 resolution.
 
-Folder testcase is contained available testcase.
-  First line is size of matrix.
-  MxN next line is available board.
-  Two last lines is start position and end position.
+#Run 
+| python main.py
---
+++ ui.py
@@ -0,0 +1,16 @@
+import pygame
+class UI:
+    def __init__(self):
+        self.normalTile = (77,179,153)
+        self.targetTile = (0,0,0)
+        self.box = (157,104,60)
+        
+    def createTile(screen, color, posX, posY):
+        if color == 'normalTile':
+            pygame.draw.rect(screen, (64,99,46), pygame.Rect(posX+1, posY+1, 58, 58),  3)   # border
+            pygame.draw.rect(screen, (77,179,153), pygame.Rect(posX+2, posY+2, 56, 56))     # inner
+        if color == 'targetTile':
+            pygame.draw.rect(screen, (0,0,0), pygame.Rect(posX, posY, 60, 60)) 
+    def createBox(screen, posX, posY):
+        pygame.draw.rect(screen, (157,104,60), pygame.Rect(posX + 4, posY + 4, 52, 52)) 
+    
