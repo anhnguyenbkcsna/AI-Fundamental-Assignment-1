a1e44c6575d42bede31e29cc2cf75d06e0b5baa5
Nguyễn Nhật Anh <ace2002luffy@gmail.com>
2023-03-03 01:35:18 +0700

    add gameplay.py

--- ui.py
+++ ui.py
@@ -10,7 +10,9 @@ class UI:
             pygame.draw.rect(screen, (64,99,46), pygame.Rect(posX+1, posY+1, 58, 58),  3)   # border
             pygame.draw.rect(screen, (77,179,153), pygame.Rect(posX+2, posY+2, 56, 56))     # inner
         if color == 'targetTile':
-            pygame.draw.rect(screen, (0,0,0), pygame.Rect(posX, posY, 60, 60)) 
+            pygame.draw.rect(screen, (0,0,0), pygame.Rect(posX, posY, 60, 60))
+        if color == 'orangeTile': # not assign num in map yet
+            pygame.draw.rect(screen, (243,113,61), pygame.Rect(posX+2, posY+2, 56, 56))     # inner
     def createBox(screen, posX, posY):
         pygame.draw.rect(screen, (157,104,60), pygame.Rect(posX + 4, posY + 4, 52, 52)) 
     
