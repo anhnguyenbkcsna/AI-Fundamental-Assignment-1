6490a38872cad8704cbb4e13920ff54ba0c33674
ChungTus <pchungtu@gmail.com>
2023-03-26 00:12:51 +0700

    demo lan 2

--- Tus.py
+++ Tus.py
@@ -10,7 +10,7 @@ pygame.init()
 pygame.font.init()
 my_font = pygame.font.SysFont('Roboto', 48)
 
-#create screeb
+#create screen
 screen = pygame.display.set_mode((1000, 800))
 pygame.display.set_caption("Bloxor")
 pygame.display.set_icon(pygame.image.load('./assets/cube64.png'))
@@ -19,54 +19,69 @@ pygame.display.set_icon(pygame.image.load('./assets/cube64.png'))
 blockImage = pygame.image.load('./assets/square.png')
 BG = pygame.image.load("img/bg.jpg")
 
+
 # testcase
-try:
-    size, boxPos, targetPos, tile = load_data("testcase/test_3.txt")
-except ValueError as e:
-    print("Error when loading testcase. Default testcase is used.")
-    size = (7, 7)  # start from 0
-    boxPos = (3, 2)  # start from 1
-    targetPos = [6, 6]  # start from 1
-    tile = [
-        [0, 0, 1, 1, 1, 1, 1],
-        [0, 0, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 0, 1],
-        [1, 1, 1, 1, 1, 1, 1],
-        [0, 1, 1, 1, 1, 1, 1],
-        [0, 0, 1, 0, 1, 1, 1],
-    ]
+# Define the function for rendering the map
+def render_map(screen, tile, size, initX, initY, unit):
+    
+    try:
+        size, boxPos, targetPos, tile = load_data("testcase/test_3.txt")
+    except ValueError as e:
+        print("Error when loading testcase. Default testcase is used.")
+        size = (7, 7)  # start from 0
+        boxPos = (3, 2)  # start from 1
+        targetPos = [6, 6]  # start from 1
+        tile = [
+            [0, 0, 1, 1, 1, 1, 1],
+            [0, 0, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1],
+            [0, 1, 1, 1, 1, 1, 1],
+            [0, 0, 1, 0, 1, 1, 1],
+        ]
+    unit = 60 # change px to square
 
-# Global variable
-unit = 60 # change px to square
+    initX = int(480 - size[0]*60/2)
+    lieX = 24
+    initY = int(380 - size[1]*60/2)
+    lieY = 14
+    standY = -32
+    blockX = initX + boxPos[0]*unit
+    blockY = initY + boxPos[1]*unit
 
-initX = int(480 - size[0]*60/2)
-lieX = 24
-blockX = initX + boxPos[0]*unit
+    # block1 always left, down and under
+    block_1x = blockX
+    block_2x = blockX
+    block_1y = blockY
+    block_2y = blockY
 
-initY = int(380 - size[1]*60/2)
-lieY = 14
-standY = -32
-blockY = initY + boxPos[1]*unit
+    targetX = initX + targetPos[0] * unit
+    targetY = initY + targetPos[1] * unit
 
-# block1 always left, down and under
-block_1x = blockX
-block_2x = blockX
-block_1y = blockY
-block_2y = blockY
+    step = 0
+    isStand = True # check if block Stand
+    keypress = ''
+    isWin = False
 
-targetX = initX + targetPos[0] * unit
-targetY = initY + targetPos[1] * unit
+    for i in range(size[0]):
+        for j in range(size[1]):
+            if tile[i][j] == 1:
+                UI.createTile(screen, 'normalTile', initX + int(i)*unit, initY + int(j)*unit)
+                UI.createBox(screen, block_1x, block_1y)
+                UI.createBox(screen, block_2x, block_2y)
+            # Other tile types...
 
-step = 0
-isStand = True # check if block Stand
-keypress = ''
-isWin = False
+# Define the function for loading the data and rendering the map
+def load_data_and_render_map(file_path, screen):
+    size, boxPos, targetPos, tile = load_data(file_path)
+    render_map(screen, tile, size, 40, 40, 60)
+    pygame.display.flip()
 
 def get_font(size): # Returns Press-Start-2P in the desired size
     return pygame.font.Font("assets/font.ttf", size)
 
-def play():
+def play(map_file):
     global step, isStand, isWin, keypress, block_1x, block_2x, block_1y, block_2y, targetX, targetY, lieX, lieY, standX, standY, blockX, blockY, initX, initY, size, boxPos, targetPos, tile
     running = True
     while running:
@@ -179,35 +194,35 @@ def levelSelect():
         screen.blit(LEVELS_TEXT, LEVELS_RECT)
 
         ONE_BUTTON = Button(image=pygame.image.load("assets/level/1.png"), pos=(220, 260), 
-                            text_input="1", font=get_font(40), base_color="#d7fcd4", hovering_color="White")
-        TWO_BUTTON = Button(image=pygame.image.load("assets/level/1.png"), pos=(360, 260), 
-                            text_input="2", font=get_font(40), base_color="#d7fcd4", hovering_color="White")
-        THREE_BUTTON = Button(image=pygame.image.load("assets/level/1.png"), pos=(500, 260), 
-                            text_input="3", font=get_font(40), base_color="#d7fcd4", hovering_color="White")
-        FOUR_BUTTON = Button(image=pygame.image.load("assets/level/1.png"), pos=(640, 260), 
-                            text_input="4", font=get_font(40), base_color="#d7fcd4", hovering_color="White")
-        FIVE_BUTTON = Button(image=pygame.image.load("assets/level/1.png"), pos=(780, 260), 
-                            text_input="5", font=get_font(40), base_color="#d7fcd4", hovering_color="White")
-        SIX_BUTTON = Button(image=pygame.image.load("assets/level/1.png"), pos=(220, 400), 
-                            text_input="6", font=get_font(40), base_color="#d7fcd4", hovering_color="White")
-        SEVEN_BUTTON = Button(image=pygame.image.load("assets/level/1.png"), pos=(360, 400), 
-                            text_input="7", font=get_font(40), base_color="#d7fcd4", hovering_color="White")
-        EIGHT_BUTTON = Button(image=pygame.image.load("assets/level/1.png"), pos=(500, 400), 
-                            text_input="8", font=get_font(40), base_color="#d7fcd4", hovering_color="White")
-        NINE_BUTTON = Button(image=pygame.image.load("assets/level/1.png"), pos=(640, 400), 
-                            text_input="9", font=get_font(40), base_color="#d7fcd4", hovering_color="White")
-        TEN_BUTTON = Button(image=pygame.image.load("assets/level/1.png"), pos=(780, 400), 
-                            text_input="10", font=get_font(40), base_color="#d7fcd4", hovering_color="White")
-        ELEV_BUTTON = Button(image=pygame.image.load("assets/level/1.png"), pos=(220, 540), 
-                            text_input="11", font=get_font(40), base_color="#d7fcd4", hovering_color="White")
-        TWELVE_BUTTON = Button(image=pygame.image.load("assets/level/1.png"), pos=(360, 540), 
-                            text_input="12", font=get_font(40), base_color="#d7fcd4", hovering_color="White")
-        THIRDTEEN_BUTTON = Button(image=pygame.image.load("assets/level/1.png"), pos=(500, 540), 
-                            text_input="13", font=get_font(40), base_color="#d7fcd4", hovering_color="White")
-        FOURTEEN_BUTTON = Button(image=pygame.image.load("assets/level/1.png"), pos=(640, 540), 
-                            text_input="14", font=get_font(40), base_color="#d7fcd4", hovering_color="White")
-        FIFTEEN_BUTTON = Button(image=pygame.image.load("assets/level/1.png"), pos=(780, 540), 
-                            text_input="15", font=get_font(40), base_color="#d7fcd4", hovering_color="White")
+                            text_input="1", font=get_font(40), base_color="#d7fcd4", hovering_color="White", level_file="testcase/test_1.txt")
+        # TWO_BUTTON = Button(image=pygame.image.load("assets/level/1.png"), pos=(360, 260), 
+        #                     text_input="2", font=get_font(40), base_color="#d7fcd4", hovering_color="White", level_file="testcase/test_1.txt")
+        # THREE_BUTTON = Button(image=pygame.image.load("assets/level/1.png"), pos=(500, 260), 
+        #                     text_input="3", font=get_font(40), base_color="#d7fcd4", hovering_color="White", level_file="testcase/test_1.txt")
+        # FOUR_BUTTON = Button(image=pygame.image.load("assets/level/1.png"), pos=(640, 260), 
+        #                     text_input="4", font=get_font(40), base_color="#d7fcd4", hovering_color="White", level_file="testcase/test_1.txt")
+        # FIVE_BUTTON = Button(image=pygame.image.load("assets/level/1.png"), pos=(780, 260), 
+        #                     text_input="5", font=get_font(40), base_color="#d7fcd4", hovering_color="White", level_file="testcase/test_1.txt")
+        # SIX_BUTTON = Button(image=pygame.image.load("assets/level/1.png"), pos=(220, 400), 
+        #                     text_input="6", font=get_font(40), base_color="#d7fcd4", hovering_color="White", level_file="testcase/test_1.txt")
+        # SEVEN_BUTTON = Button(image=pygame.image.load("assets/level/1.png"), pos=(360, 400), 
+        #                     text_input="7", font=get_font(40), base_color="#d7fcd4", hovering_color="White", level_file="testcase/test_1.txt")
+        # EIGHT_BUTTON = Button(image=pygame.image.load("assets/level/1.png"), pos=(500, 400), 
+        #                     text_input="8", font=get_font(40), base_color="#d7fcd4", hovering_color="White", level_file="testcase/test_1.txt")
+        # NINE_BUTTON = Button(image=pygame.image.load("assets/level/1.png"), pos=(640, 400), 
+        #                     text_input="9", font=get_font(40), base_color="#d7fcd4", hovering_color="White", level_file="testcase/test_1.txt")
+        # TEN_BUTTON = Button(image=pygame.image.load("assets/level/1.png"), pos=(780, 400), 
+        #                     text_input="10", font=get_font(40), base_color="#d7fcd4", hovering_color="White", level_file="testcase/test_1.txt")
+        # ELEV_BUTTON = Button(image=pygame.image.load("assets/level/1.png"), pos=(220, 540), 
+        #                     text_input="11", font=get_font(40), base_color="#d7fcd4", hovering_color="White", level_file="testcase/test_1.txt")
+        # TWELVE_BUTTON = Button(image=pygame.image.load("assets/level/1.png"), pos=(360, 540), 
+        #                     text_input="12", font=get_font(40), base_color="#d7fcd4", hovering_color="White", level_file="testcase/test_1.txt")
+        # THIRDTEEN_BUTTON = Button(image=pygame.image.load("assets/level/1.png"), pos=(500, 540), 
+        #                     text_input="13", font=get_font(40), base_color="#d7fcd4", hovering_color="White", level_file="testcase/test_1.txt")
+        # FOURTEEN_BUTTON = Button(image=pygame.image.load("assets/level/1.png"), pos=(640, 540), 
+        #                     text_input="14", font=get_font(40), base_color="#d7fcd4", hovering_color="White", level_file="testcase/test_1.txt")
+        # FIFTEEN_BUTTON = Button(image=pygame.image.load("assets/level/1.png"), pos=(780, 540), 
+        #                     text_input="15", font=get_font(40), base_color="#d7fcd4", hovering_color="White", level_file="testcase/test_1.txt")
        
 
 
@@ -224,8 +239,11 @@ def levelSelect():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if LEVELS_BACK.checkForInput(LEVELS_MOUSE_POS):
                     main_menu()
-
-        for button in [ONE_BUTTON, TWO_BUTTON, THREE_BUTTON, FOUR_BUTTON, FIVE_BUTTON,  SIX_BUTTON, SEVEN_BUTTON, EIGHT_BUTTON, NINE_BUTTON, TEN_BUTTON, ELEV_BUTTON, TWELVE_BUTTON, THIRDTEEN_BUTTON, FOURTEEN_BUTTON, FIFTEEN_BUTTON]:
+                if ONE_BUTTON.checkForInput(LEVELS_MOUSE_POS):
+                    # call load_data_and_render_map function to render the map for level_one.txt
+                    load_data_and_render_map(ONE_BUTTON.level_file, screen)
+#, TWO_BUTTON, THREE_BUTTON, FOUR_BUTTON, FIVE_BUTTON,  SIX_BUTTON, SEVEN_BUTTON, EIGHT_BUTTON, NINE_BUTTON, TEN_BUTTON, ELEV_BUTTON, TWELVE_BUTTON, THIRDTEEN_BUTTON, FOURTEEN_BUTTON, FIFTEEN_BUTTON
+        for button in [ONE_BUTTON]:
             button.changeColor(pygame.mouse.get_pos())
             button.update(screen)
         pygame.display.update()
@@ -257,8 +275,8 @@ def main_menu():
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
-                    play()
+                # if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                #     play()
                 if LEVELS_BUTTON.checkForInput(MENU_MOUSE_POS):
                     levelSelect()
                 if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
--- button.py
+++ button.py
@@ -1,6 +1,7 @@
 import pygame
+
 class Button():
-	def __init__(self, image, pos, text_input, font, base_color, hovering_color):
+	def __init__(self, image, pos, text_input, font, base_color, hovering_color, level_file=None):
 		self.image = image
 		self.x_pos = pos[0]
 		self.y_pos = pos[1]
@@ -12,6 +13,8 @@ class Button():
 			self.image = self.text
 		self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
 		self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
+		self.level_file = level_file
+
 
 	def update(self, screen):
 		if self.image is not None:
@@ -28,35 +31,3 @@ class Button():
 			self.text = self.font.render(self.text_input, True, self.hovering_color)
 		else:
 			self.text = self.font.render(self.text_input, True, self.base_color)
-
-class SpriteObject(pygame.sprite.Sprite):
-    def __init__(self, x, y, filename):
-        super().__init__() 
-        img = pygame.image.load(filename).convert_alpha()
-        self.original_image = pygame.Surface((60, 60))
-        self.original_image.blit(img, img.get_rect(center = self.original_image.fill((127, 127, 127)).center))
-        self.hover_image = pygame.Surface((60, 60))
-        self.hover_image.blit(img, img.get_rect(center = self.hover_image.fill((228, 228, 228)).center))
-        self.image = self.original_image 
-        self.rect = self.image.get_rect(center = (x, y))
-        self.hover = False
-
-    def update(self):
-        self.hover = self.rect.collidepoint(pygame.mouse.get_pos())
-        self.image = self.hover_image if self.hover else self.original_image
-	
-class Level(pygame.sprite.Sprite):
-    def __init__(self, x, y, filename):
-        super().__init__() 
-        img = pygame.image.load(filename).convert_alpha()
-        self.original_image = pygame.Surface((70, 70))
-        self.original_image.blit(img, img.get_rect(center = self.original_image.fill((127, 127, 127)).center))
-        self.hover_image = pygame.Surface((70, 70))
-        self.hover_image.blit(img, img.get_rect(center = self.hover_image.fill((228, 228, 228)).center))
-        self.image = self.original_image 
-        self.rect = self.image.get_rect(center = (x, y))
-        self.hover = False
-
-    def update(self):
-        self.hover = self.rect.collidepoint(pygame.mouse.get_pos())
-        self.image = self.hover_image if self.hover else self.original_image	
