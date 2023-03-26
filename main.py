5c564c10fa944392038aae6ca410635c64215fcb
Nguyễn Nhật Anh <ace2002luffy@gmail.com>
2023-03-26 23:48:39 +0700

    update BFS

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pygame
-import sys
+import getopt, sys
 from ui import *
-from gameplay import *
 from utils import load_data
 
 sys.path.insert(0, '/ui.py')
@@ -12,7 +11,7 @@ pygame.font.init()
 my_font = pygame.font.SysFont('Roboto', 48)
 
 
-#create the screen
+# create the screen
 screen = pygame.display.set_mode((1000, 800))
 pygame.display.set_caption("Bloxor")
 pygame.display.set_icon(pygame.image.load('./assets/cube64.png'))
@@ -22,12 +21,12 @@ blockImage = pygame.image.load('./assets/square.png')
 
 # testcase
 try:
-    size, boxPos, targetPos, tile = load_data("testcase/test_3.txt")
+    size, boxPos, targetPos, tile = load_data("testcase/test_2.txt")
 except ValueError as e:
     print("Error when loading testcase. Default testcase is used.")
     size = (7, 7)  # start from 0
     boxPos = (3, 2)  # start from 1
-    targetPos = [6, 6]  # start from 1
+    targetPos = (6, 6)  # start from 1
     tile = [
         [0, 0, 1, 1, 1, 1, 1],
         [0, 0, 1, 1, 1, 1, 1],
@@ -39,7 +38,7 @@ except ValueError as e:
     ]
 
 # Global variable
-unit = 60 # change px to square
+unit = 60  # change px to square
 
 initX = int(480 - size[0]*60/2)
 lieX = 24
@@ -51,185 +50,220 @@ standY = -32
 blockY = initY + boxPos[1]*unit
 
 # block1 always left, down and under
-block_1x = blockX
-block_2x = blockX
-block_1y = blockY
-block_2y = blockY
+currentPos1 = boxPos
+currentPos2 = boxPos
 
 targetX = initX + targetPos[0] * unit
 targetY = initY + targetPos[1] * unit
 
 step = 0
-isStand = True # check if block Stand
+isStand = True  # check if block Stand
 keypress = ''
 isWin = False
 
-def checkWin():
+def checkOutMap(currentPos1, currentPos2, tile):
+    if currentPos1[0] < 0 or currentPos2[0] < 0 or currentPos1[1] < 0 or currentPos2[1] < 0:
+        return True
+    if currentPos1[0] >= size[0] or currentPos2[0] >= size[0] or currentPos1[1] >= size[1] or currentPos2[1] >= size[1]:
+        return True
+    if tile[currentPos1[1]][currentPos1[0]] == 0 or tile[currentPos2[1]][currentPos2[0]] == 0:
+        return True
     return False
-def checkBorder(block_1x, block_1y, block_2x, block_2y, tile):
-    return True
 
-def bfs(blockX, blockY, targetX, targetY, tile):
+def bfs(currentPos1, currentPos2, tile):
     queue = []
+    move = []
     visit = []
-    queue.append((blockX, blockY, blockX, blockY))
-    while len(queue) > 0:
-        print(queue, visit)
+    visit.append((currentPos1, currentPos2))
+    queue.append((currentPos1, currentPos2))
+    while len(queue) > 0 or len(visit) >= size[0] * size[1] or isWin == True:
+        print('Queue:', queue)
+        print('Visit:', visit)
         current = queue.pop(0) # FIFO
-        if current[0] == targetX and current [2] == targetX and current[1] == targetY and current[3] == targetY:
-            return current
-        block_1x = current[0]
-        block_1y = current[1]
-        block_2x = current[2]
-        block_2y = current[3]
-        
+        print('Current: ', current)
+        #checkWin
+        if((targetPos, targetPos) in visit):
+            queue.append((targetPos, targetPos))
+            print('WIN !!!')
+            return True
         # move 4 direction -> BFS
-            # move up
-        if block_1y == block_2y:
-            if block_1x == block_2x:
-                block_2y -= unit
-                block_1y = block_2y - unit
+            # move up down
+        pos1_up, pos2_up, pos1_down, pos2_down = 0, 0, 0, 0
+        pos1_left, pos2_left, pos1_right, pos2_right = 0, 0, 0, 0
+        if currentPos1[1] == currentPos2[1]: # cùng y -- 
+            if currentPos1[0] == currentPos2[0]: # 1 block to 2 block
+                pos2_up = (currentPos2[0], currentPos2[1] - 1)
+                pos1_up = (currentPos1[0], currentPos1[1] - 2)
+                pos1_down = (currentPos1[0], currentPos1[1] + 1)
+                pos2_down = (currentPos2[0], currentPos2[1] + 2)
             else:
-                block_1y -= unit
-                block_2y -= unit
-        else:
-            block_1y -= unit
-            block_2y = block_1y
-        # check border
-        if (block_1x, block_1y, block_2x, block_2y) not in visit:
-            visit.append((block_1x, block_1y, block_2x, block_2y))
-            queue.append((block_1x, block_1y, block_2x, block_2y))
-            # move down
-        if block_1y == block_2y:
-            if block_1x == block_2x: # stand
-                block_1y += unit
-                block_2y = block_1y + unit
+                pos1_up = (currentPos1[0], currentPos1[1] - 1)
+                pos2_up = (currentPos2[0], currentPos2[1] - 1)
+                pos1_down = (currentPos1[0], currentPos1[1] + 1)
+                pos2_down = (currentPos2[0], currentPos2[1] + 1)
+        else: # 2 block to 1 block
+            pos1_up = (currentPos1[0], currentPos1[1] - 1)
+            pos2_up = (currentPos2[0], currentPos2[1] - 2)
+            pos2_down = (currentPos2[0], currentPos2[1] + 1)
+            pos1_down = (currentPos1[0], currentPos1[1] + 2)
+        # print('pos1_up, pos2_up, pos1_down, pos2_down', pos1_up, pos2_up, pos1_down, pos2_down)
+        
+        # move left right
+        if currentPos1[0] == currentPos2[0]:
+            if currentPos1[1] == currentPos2[1]:
+                pos2_left = (currentPos2[0] - 1, currentPos2[1])
+                pos1_left = (currentPos2[0] - 2, currentPos1[1])
+                pos1_right = (currentPos1[0] + 1, currentPos1[1])
+                pos2_right = (currentPos1[0] + 2, currentPos2[1])                
             else:
-                block_1y += unit
-                block_2y += unit
+                pos1_left = (currentPos1[0] - 1, currentPos1[1])
+                pos2_left = (currentPos2[0] - 1, currentPos2[1])
+                pos1_right = (currentPos1[0] + 1, currentPos1[1])
+                pos2_right = (currentPos2[0] + 1, currentPos2[1])
         else:
-            block_2y += unit
-            block_1y = block_2y
-        if (block_1x, block_1y, block_2x, block_2y) not in visit:
-            visit.append((block_1x, block_1y, block_2x, block_2y))
-            queue.append((block_1x, block_1y, block_2x, block_2y))
-            # move left
-        if block_1x == block_2x:
-            if block_1y == block_2y:
-                block_2x -= unit
-                block_1x = block_2x - unit
-            else:
-                block_1x -= unit
-                block_2x -= unit
-        else:
-            block_1x -= unit
-            block_2x = block_1x
-        if (block_1x, block_1y, block_2x, block_2y) not in visit:
-            visit.append((block_1x, block_1y, block_2x, block_2y))
-            queue.append((block_1x, block_1y, block_2x, block_2y))
-            # move right
-        if block_1x == block_2x:
-            if block_1y == block_2y:
-                block_1x += unit
-                block_2x = block_1x + unit
-            else:
-                block_1x += unit
-                block_2x += unit
-        else:
-            block_2x += unit
-            block_1x = block_2x
-        if (block_1x, block_1y, block_2x, block_2y) not in visit:
-            visit.append((block_1x, block_1y, block_2x, block_2y))
-            queue.append((block_1x, block_1y, block_2x, block_2y))
-    return None
-
+            pos1_left = (currentPos1[0] - 1, currentPos1[1])
+            pos2_left = (currentPos1[0] - 1, currentPos2[1])
+            pos2_right = (currentPos2[0] + 1, currentPos2[1])
+            pos1_right = (currentPos2[0] + 1, currentPos1[1])
+        # checkOutmap
+            # check visit
+                # append to Queue and Visit
+        if not checkOutMap(pos1_up, pos2_up, tile): #continue
+            if (pos1_up, pos2_up) not in visit:
+                currentPos1 = pos1_up
+                currentPos2 = pos2_up
+                move.append('U')
+                queue.append((pos1_up, pos2_up))
+                visit.append((pos1_up, pos2_up))
+        if not checkOutMap(pos1_down, pos2_down, tile): #continue
+            if (pos1_down, pos2_down) not in visit:
+                currentPos1 = pos1_down
+                currentPos2 = pos2_down
+                move.append('D')
+                queue.append((pos1_down, pos2_down))
+                visit.append((pos1_down, pos2_down))
+        if not checkOutMap(pos1_left, pos2_left, tile): #continue
+            if (pos1_left, pos2_left) not in visit:
+                currentPos1 = pos1_left
+                currentPos2 = pos2_left
+                move.append('L')
+                queue.append((pos1_left, pos2_left))
+                visit.append((pos1_left, pos2_left))
+        if not checkOutMap(pos1_right, pos2_right, tile): #continue
+            if (pos1_right, pos2_right) not in visit:
+                currentPos1 = pos1_right
+                currentPos2 = pos2_right
+                move.append('R')
+                queue.append((pos1_right, pos2_right))
+                visit.append((pos1_right, pos2_right))
+        print(move)
+        print('-------------')
+    return False
 # Game loop
+# if sys.argv[0] == 'BFS'  or sys.argv[0] == 'bfs':
 running = True
 while running:
-    screen.fill((255,255,255)) # background
-    # stepText = my_font.render('Move {}'.format(step), False, (0, 0, 0))
-    stepText = my_font.render('{} {} {} {}'.format(block_1x, block_2x, block_1y, block_2y), False, (0, 0, 0))
-    stepText = my_font.render('Move {}'.format(step), False, (0, 0, 0))
-    # stepText = my_font.render('{} {} {} {}'.format(block_1x, block_2x, block_1y, block_2y), False, (0, 0, 0))
-    screen.blit(stepText, (430,100))
-    # stepText = my_font.render('{} {} {} {}'.format(block_1x, block_1y, block_2x, block_2y), False, (0, 0, 0))
-    # screen.blit(stepText, (430,100))
-    bfs(blockX, blockY, targetX, targetY, tile)
-    # for event in pygame.event.get():
-    #     # quit game
-    #     if event.type == pygame.QUIT:
-    #         running = False
-    #     # Move block
-    #     if event.type == pygame.KEYDOWN:
-    #         keypress = event.key
-    #         # Gameplay -> class Move
-    #         if block_1y > block_2y:
-    #             (block_1y, block_2y) = (block_2y, block_1y)
-    #         if keypress == pygame.K_a or keypress == pygame.K_LEFT:
-    #             if block_1x == block_2x:
-    #                 if block_1y == block_2y:
-    #                     block_2x -= unit
-    #                     block_1x = block_2x - unit
-    #                 else:
-    #                     block_1x -= unit
-    #                     block_2x -= unit
-    #             else:
-    #                 block_1x -= unit
-    #                 block_2x = block_1x
-    #             step += 1
-    #         if keypress == pygame.K_d or keypress == pygame.K_RIGHT:
-    #             if block_1x == block_2x:
-    #                 if block_1y == block_2y:
-    #                     block_1x += unit
-    #                     block_2x = block_1x + unit
-    #                 else:
-    #                     block_1x += unit
-    #                     block_2x += unit
-    #             else:
-    #                 block_2x += unit
-    #                 block_1x = block_2x
-    #             step += 1
-    #         if keypress == pygame.K_w or keypress == pygame.K_UP:
-    #             if block_1y == block_2y:
-    #                 if block_1x == block_2x:
-    #                     block_2y -= unit
-    #                     block_1y = block_2y - unit
-    #                 else:
-    #                     block_1y -= unit
-    #                     block_2y -= unit
-    #             else:
-    #                 block_1y -= unit
-    #                 block_2y = block_1y
-    #             step += 1
-    #         if keypress == pygame.K_s or keypress == pygame.K_DOWN:
-    #             if block_1y == block_2y:
-    #                 if block_1x == block_2x: # stand
-    #                     block_1y += unit
-    #                     block_2y = block_1y + unit
-    #                 else:
-    #                     block_1y += unit
-    #                     block_2y += unit
-    #             else:
-    #                 block_2y += unit
-    #                 block_1y = block_2y
-    #             step += 1
-    #         if keypress == pygame.K_r: # reset round
-    #             blockX = initX + boxPos[0]*unit
-    #             blockY = initY + boxPos[1]*unit
-    #             step = 0
-    #         # if event.key == pygame.K_q: # back step <- history
+    screen.fill((255, 255, 255))  # background
 
-    # check Win
-    if block_1x == targetX and block_2x == targetX and block_1y == targetY and block_2y == targetY:
-        isWin = True
+    stepText = my_font.render('{} {} {} {}'.format(currentPos1[0], currentPos1[1], currentPos2[0], currentPos2[1]), False, (0, 0, 0))
+    screen.blit(stepText, (430, 100))
+    
+    # check WIN & LOSE
+    if checkOutMap(currentPos1, currentPos2, tile):
+        gameStatus = my_font.render('You lose!', True, (255, 0, 0))
+        screen.blit(gameStatus, (415, 50))
+            # check Win
+    elif currentPos1 == targetPos and currentPos2 == targetPos:
+        gameStatus = my_font.render('You win!', True, (0, 185, 0))
+        screen.blit(gameStatus, (415, 50))
+        
+        
+    # if(bfs(currentPos1, currentPos2, tile)):
+    #     running = False
+    # else: 
+    #     print('Can\'t solve!')
+    #     running = False
+    
+    
+    for event in pygame.event.get():
+        # quit game
+        if event.type == pygame.QUIT:
+            running = False
+        # Move block
+        if event.type == pygame.KEYDOWN:
+            keypress = event.key
+            # Gameplay -> class Move
+            if currentPos1[0] > currentPos2[0]:
+                currentPos1[0], currentPos2[0] = currentPos2[0], currentPos1[0]
+            if currentPos1[1] > currentPos2[1]:
+                currentPos1[1], currentPos2[1] = currentPos2[1], currentPos1[1]
+            if keypress == pygame.K_a or keypress == pygame.K_LEFT:
+                if currentPos1[0] == currentPos2[0]:
+                    if currentPos1[1] == currentPos2[1]:
+                        currentPos2 = (currentPos2[0] - 1, currentPos2[1])
+                        currentPos1 = (currentPos2[0] - 1, currentPos1[1])
+                    else:
+                        currentPos1 = (currentPos1[0] - 1, currentPos1[1])
+                        currentPos2 = (currentPos2[0] - 1, currentPos2[1])
+                else:
+                    currentPos1 = (currentPos1[0] - 1, currentPos1[1])
+                    currentPos2 = (currentPos1[0], currentPos2[1])
+                step += 1
+            if keypress == pygame.K_d or keypress == pygame.K_RIGHT:
+                if currentPos1[0] == currentPos2[0]:
+                    if currentPos1[1] == currentPos2[1]:
+                        currentPos1 = (currentPos1[0] + 1, currentPos1[1])
+                        currentPos2 = (currentPos1[0] + 1, currentPos2[1])
+                    else:
+                        currentPos1 = (currentPos1[0] + 1, currentPos1[1])
+                        currentPos2 = (currentPos2[0] + 1, currentPos2[1])
+                else:
+                    currentPos2 = (currentPos2[0] + 1, currentPos2[1])
+                    currentPos1 = (currentPos2[0], currentPos1[1])
+                step += 1
+            if keypress == pygame.K_w or keypress == pygame.K_UP:
+                if currentPos1[1] == currentPos2[1]:
+                    if currentPos1[0] == currentPos2[0]:
+                        currentPos2 = (currentPos2[0], currentPos2[1] - 1)
+                        currentPos1 = (currentPos1[0], currentPos2[1] - 1)
+                    else:
+                        currentPos1 = (currentPos1[0], currentPos1[1] - 1)
+                        currentPos2 = (currentPos2[0], currentPos2[1] - 1)
+                else:
+                    currentPos1 = (currentPos1[0], currentPos1[1] - 1)
+                    currentPos2 = (currentPos2[0], currentPos1[1])
+                step += 1
+            if keypress == pygame.K_s or keypress == pygame.K_DOWN:
+                if currentPos1[1] == currentPos2[1]:
+                    if currentPos1[0] == currentPos2[0]:  # stand
+                        currentPos1 = (currentPos1[0], currentPos1[1] + 1)
+                        currentPos2 = (currentPos2[0], currentPos1[1] + 1)
+                    else:
+                        currentPos1 = (currentPos1[0], currentPos1[1] + 1)
+                        currentPos2 = (currentPos2[0], currentPos2[1] + 1)
+                else:
+                    currentPos2 = (currentPos2[0], currentPos2[1] + 1)
+                    currentPos1 = (currentPos1[0], currentPos2[1])
+                step += 1
+            if keypress == pygame.K_r:  # reset round
+                blockX = initX + boxPos[0]*unit
+                blockY = initY + boxPos[1]*unit
+                currentPos1 = boxPos
+                currentPos2 = boxPos
+                step = 0
+        # print(currentPos1, currentPos2)
+    # if event.key == pygame.K_q: # back step <- history
+
+
     # draw map & block
     for i in range(size[0]):
         for j in range(size[1]):
             if tile[i][j] == 1:
-                UI.createTile(screen, 'normalTile', initX + int(i)*unit, initY + int(j)*unit)
+                UI.createTile(screen, 'normalTile', initX +
+                            int(j)*unit, initY + int(i)*unit)
     UI.createTile(screen, 'targetTile', targetX, targetY)
-    UI.createBox(screen, block_1x, block_1y)
-    UI.createBox(screen, block_2x, block_2y)
+    UI.createBox(screen, initX + currentPos1[0]*unit, initY + currentPos1[1]*unit)
+    UI.createBox(screen, initX + currentPos2[0]*unit, initY + currentPos2[1]*unit)
 
     # Gameplay.checkWin(blockX, blockY, boxPos)
     pygame.display.update()
